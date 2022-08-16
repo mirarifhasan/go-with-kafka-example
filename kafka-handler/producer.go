a7b78d8eecd71fd4bfbf430155d788c9fbc515f5
mirarifhasan <arif.ishan05@gmail.com>
2022-08-16 22:43:20 +0600

    feat: gin with kafka

--- kafka-handler/producer.go
+++ kafka-handler/producer.go
@@ -4,32 +4,13 @@ import (
 	"context"
 	"github.com/segmentio/kafka-go"
 	"log"
-	"time"
 )
 
-const (
-	topic         = "message-log"
-	brokerAddress = "localhost:9092"
-	partition     = 0
-)
-
-func Produce(ctx context.Context) {
-	conn, err := kafka.DialLeader(context.Background(), "tcp", brokerAddress, topic, partition)
-	if err != nil {
-		log.Fatal("failed to dial leader:", err)
-	}
-
-	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	_, err = conn.WriteMessages(
-		kafka.Message{Value: []byte("one!")},
-		kafka.Message{Value: []byte("two!")},
-		kafka.Message{Value: []byte("three!")},
+func Produce(ctx context.Context, msg string) {
+	_, err := writer.WriteMessages(
+		kafka.Message{Value: []byte(msg)},
 	)
 	if err != nil {
 		log.Fatal("failed to write messages:", err)
-	}
-
-	if err := conn.Close(); err != nil {
-		log.Fatal("failed to close writer:", err)
 	}
 }
